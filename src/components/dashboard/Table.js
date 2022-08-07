30cfb5f11c007ad666ede69695b6ef6916b1c16b
becca09 <adeolaadefuye@gmail.com>
2022-08-07 20:43:01 +0100

    worked on, dashboard, landing page, and registration

--- src/components/dashboard/Table.js
+++ src/components/dashboard/Table.js
@@ -1,27 +1,71 @@
 import React from 'react';
+import Button from "../reusables/Button";
 
 const Table = () => {
     return (
         <div>
             <div className="table">
                 <table>
+                    <tbody>
                     <tr>
-                        <th>Company</th>
-                        <th>Contact</th>
-                        <th>Country</th>
+                        <th>S/N</th>
+                        <th>Name</th>
+                        <th>Email</th>
                     </tr>
                     <tr>
-                        <td>Alfreds Futterkiste</td>
-                        <td>Maria Anders</td>
-                        <td>Germany</td>
+                        <td>1</td>
+                        <td>Deola Deola</td>
+                        <td>deola@gmail.com</td>
                     </tr>
                     <tr>
-                        <td>Centro comercial Moctezuma</td>
-                        <td>Francisco Chang</td>
-                        <td>Mexico</td>
+                        <td>2</td>
+                        <td>Tolani</td>
+                        <td>tolani@gmail.com</td>
                     </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Tolani</td>
+                        <td>tolani@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>Tolani</td>
+                        <td>tolani@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td>Tolani</td>
+                        <td>tolani@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td>6</td>
+                        <td>Tolani</td>
+                        <td>tolani@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td>7</td>
+                        <td>Tolani</td>
+                        <td>tolani@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td>8</td>
+                        <td>Tolani</td>
+                        <td>tolani@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td>9</td>
+                        <td>Tolani</td>
+                        <td>tolani@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>Tolani</td>
+                        <td>tolani@gmail.com</td>
+                    </tr>
+                    </tbody>
                 </table>
             </div>
+            <Button  type={"button"} buttonStyle={"tableButton"}>Generate Matches</Button>
         </div>
     );
 };
